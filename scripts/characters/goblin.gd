ddc4aa5febdfa3f34b2df5e2e486b00d15725f26
fdeviruz <furiadeviruz@gmail.com>
2021-06-03 16:41:57 -0400

    Goblin run animation

--- scripts/characters/goblin.gd
+++ scripts/characters/goblin.gd
@@ -2,26 +2,32 @@ extends KinematicBody2D
 
 export (int) var speed = 150
 
-var _target = Vector2(randi() % 1280, randi() % 720)
+var _target
 var _timer = null
 var _velocity = Vector2()
 
+onready var Animation: AnimatedSprite = get_node("AnimatedSprite")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_target = position
 	_timer = Timer.new()
 	add_child(_timer)
 	_timer.connect("timeout", self, "move_random")
 	_timer.set_wait_time(4)
 	_timer.set_one_shot(false)
 	_timer.start()
-		
+	move_random()
 		
 func _physics_process(delta):
 	_velocity = position.direction_to(_target) * speed
 	# look_at(target)
 	if position.distance_to(_target) > 5:
 		_velocity = move_and_slide(_velocity)
+	else:
+		Animation.animation = "default"
 
 
 func move_random():
 	_target = Vector2(randi() % 1280, randi() % 720)
+	Animation.animation = "move"
